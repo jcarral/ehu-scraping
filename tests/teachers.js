9123be40f00539d861b41cec6cd94361fb16f205
Joseba <jcarraldc@gmail.com>
2018-01-17 17:51:55 +0100

    Teacher tests

---
+++ tests/teachers.js
@@ -0,0 +1,66 @@
+const chai = require('chai');
+const { expect, assert } = chai;
+const chaiAsPromised = require('chai-as-promised');
+
+const { getTeachersByGrade, getTeacherSchedule } = require('../');
+
+chai.use(chaiAsPromised);
+
+describe('getTeacherByGrade', () => {
+	it('should return an error, grade code is required', () => {
+		return expect(getTeachersByGrade()).to.be.rejected;
+	});
+
+	it('should return an array array with the list of teachers', () => {
+		return getTeachersByGrade('GINFOR20')
+			.then(res => {
+				expect(res).to.be.an('object');
+				expect(res).to.have.property('school');
+				expect(res).to.have.property('grade');
+				expect(res).to.have.property('teachers');
+				expect(res.teachers).to.be.an('array');
+				expect(res.teachers).to.have.members();
+				const teacher = res.teachers[0];
+				expect(teacher).to.be.an('object');
+				expect(teacher).to.have.property('name');
+				expect(teacher).to.have.property('id');
+				expect(teacher).to.have.property('email');
+				expect(teacher).to.have.property('href');
+			});
+	});
+});
+
+describe('getTeacherSchedule', () => {
+
+	it('should return an error, id is required', () => {
+		return expect(getTeacherSchedule('GINFOR20')).to.be.rejected;
+	});
+
+	it('should return an error, grade is required', () => {
+		return expect(getTeacherSchedule(null, '4150')).to.be.rejected;
+	});
+
+	it('should return an error, all parameters are required', () => {
+		return expect(getTeacherSchedule()).to.be.rejected;
+	});
+
+	it('should return the teacher schedule object', () => {
+		return getTeacherSchedule('GINFOR20', '4150').then(res => {
+			expect(res).to.be.an('object');
+			expect(res).to.have.property('name');
+			expect(res).to.have.property('category');
+			expect(res).to.have.property('departament');
+			expect(res).to.have.property('area');
+			expect(res).to.have.property('id');
+			expect(res).to.have.property('email');
+			expect(res).to.have.property('schedule');
+			expect(res.schedule).to.be.an('array');
+			expect(res.schedule).to.have.members();
+			const tutorship = res.schedule[0];
+			expect(tutorship).to.be.an('object');
+			expect(tutorship).to.have.property('date'); //TODO: Validate type YYYY-MM-DDTHH:MM
+			expect(tutorship).to.have.property('place');
+		});
+	});
+
+});
